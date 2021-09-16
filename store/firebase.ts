41e795a5e2ddff7988ce43a536482f0c0f486af4
Sharad Chand <pepsighan@sharadchand.com>
2021-09-16 10:13:59 +0545

    Initialize firebase

---
+++ store/firebase.ts
@@ -0,0 +1,17 @@
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCDwQ0M7zYZPfLH3hA3RT4NVm4azzxQ_PI',
+  authDomain: 'graftini-dev-60c2a.firebaseapp.com',
+  projectId: 'graftini-dev-60c2a',
+  storageBucket: 'graftini-dev-60c2a.appspot.com',
+  messagingSenderId: '472653453897',
+  appId: '1:472653453897:web:d265ec9b1bed7daefb7d98',
+  measurementId: 'G-WYCC40W1HM',
+};
+
+export const app = initializeApp(firebaseConfig);
+export const analytics = getAnalytics(app);
+export const firestore = getFirestore(app);
