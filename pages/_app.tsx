0f09d7ffec4f3d945042ee3323aea2c6ad473b18
Sharad Chand <pepsighan@sharadchand.com>
2021-09-16 20:08:31 +0545

    Reset query client when user auth changes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,22 +1,30 @@
 import { CssBaseline, GlobalStyles, ThemeProvider } from '@mui/material';
+import { useCallback, useMemo } from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
-import { useInitializeAuth } from 'store/auth';
+import { useAuth, useInitializeAuth } from 'store/auth';
 import theme from 'utils/theme';
-
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      // Do not automatically keep on refetching the same data.
-      staleTime: Infinity,
-    },
-  },
-});
 
 export default function MyApp({ Component, pageProps }) {
   useInitializeAuth();
 
+  const uid = useAuth(useCallback((state) => state.user?.uid, []));
+  const queryClient = useMemo(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            // Do not automatically keep on refetching the same data.
+            staleTime: Infinity,
+          },
+        },
+      }),
+    // When the user changes, reset the query cache in the client.
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [uid]
+  );
+
   return (
-    <QueryClientProvider client={queryClient}>
+    <QueryClientProvider key={uid} client={queryClient}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <GlobalStyles
