bd2e109ad61132f05521b989e7be7db64976d7c3
Sharad Chand <pepsighan@sharadchand.com>
2021-09-17 17:00:44 +0545

    Implement dragging from the toolbox

--- components/designer/WidgetButton.tsx
+++ components/designer/WidgetButton.tsx
@@ -1,8 +1,18 @@
-import { Button, Box } from '@mui/material';
+import { useEditor } from '@craftjs/core';
+import { Box, Button } from '@mui/material';
+import Title from 'components/prebuilt/Title';
+import { useCallback } from 'react';
 
 export default function WidgetButton() {
+  const {
+    connectors: { create },
+  } = useEditor();
+
   return (
-    <Button sx={{ height: 150 }}>
+    <Button
+      ref={useCallback((ref) => create(ref, <Title />), [create])}
+      sx={{ height: 150 }}
+    >
       <Box sx={{ width: 100, height: 100, border: '1px solid' }} />
     </Button>
   );
